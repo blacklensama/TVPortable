ab57bb7f83640c6c72dc34db668841a8ce47838e
Robert Bu <darkfall@gamemastercn.com>
2012-06-17 19:12:07 +0800

    add osx keycode to CCKey::keycode keymap

--- cocos2dx/platform/mac/CCMacEventDelegate.mm
+++ cocos2dx/platform/mac/CCMacEventDelegate.mm
@@ -18,6 +18,90 @@ using namespace cocos2d;
 // because i do not want this delegate to be included in other source files
 static CCMacEventDelegate* delegate = [[CCMacEventDelegate alloc] init];
 
+// mac osx keycode to cc keycode
+static int __key_map[128] = { 0 };
+
+- (void)__init_key_map {
+    __key_map[kVK_ANSI_A] = CCKey::A;
+    __key_map[kVK_ANSI_B] = CCKey::B;
+    __key_map[kVK_ANSI_C] = CCKey::C;
+    __key_map[kVK_ANSI_D] = CCKey::D;
+    __key_map[kVK_ANSI_E] = CCKey::E;
+    __key_map[kVK_ANSI_F] = CCKey::F;
+    __key_map[kVK_ANSI_G] = CCKey::G;
+    __key_map[kVK_ANSI_H] = CCKey::H;
+    __key_map[kVK_ANSI_I] = CCKey::I;
+    __key_map[kVK_ANSI_J] = CCKey::J;
+    __key_map[kVK_ANSI_K] = CCKey::K;
+    __key_map[kVK_ANSI_L] = CCKey::L;
+    __key_map[kVK_ANSI_M] = CCKey::M;
+    __key_map[kVK_ANSI_N] = CCKey::N;
+    __key_map[kVK_ANSI_O] = CCKey::O;
+    __key_map[kVK_ANSI_P] = CCKey::P;
+    __key_map[kVK_ANSI_Q] = CCKey::Q;
+    __key_map[kVK_ANSI_R] = CCKey::R;
+    __key_map[kVK_ANSI_S] = CCKey::S;
+    __key_map[kVK_ANSI_T] = CCKey::T;
+    __key_map[kVK_ANSI_U] = CCKey::U;
+    __key_map[kVK_ANSI_V] = CCKey::V;
+    __key_map[kVK_ANSI_W] = CCKey::W;
+    __key_map[kVK_ANSI_X] = CCKey::X;
+    __key_map[kVK_ANSI_Y] = CCKey::Y;
+    __key_map[kVK_ANSI_Z] = CCKey::Z;
+    
+    __key_map[kVK_ANSI_1] = CCKey::Num1;
+    __key_map[kVK_ANSI_2] = CCKey::Num2;
+    __key_map[kVK_ANSI_3] = CCKey::Num3;
+    __key_map[kVK_ANSI_4] = CCKey::Num4;
+    __key_map[kVK_ANSI_5] = CCKey::Num5;
+    __key_map[kVK_ANSI_6] = CCKey::Num6;
+    __key_map[kVK_ANSI_7] = CCKey::Num7;
+    __key_map[kVK_ANSI_8] = CCKey::Num8;
+    __key_map[kVK_ANSI_9] = CCKey::Num9;
+    __key_map[kVK_ANSI_0] = CCKey::Num0;
+    
+    __key_map[kVK_F1] = CCKey::F1;
+    __key_map[kVK_F2] = CCKey::F2;
+    __key_map[kVK_F3] = CCKey::F3;
+    __key_map[kVK_F4] = CCKey::F4;
+    __key_map[kVK_F5] = CCKey::F5;
+    __key_map[kVK_F6] = CCKey::F6;
+    __key_map[kVK_F7] = CCKey::F7;
+    __key_map[kVK_F8] = CCKey::F8;
+    __key_map[kVK_F9] = CCKey::F9;
+    __key_map[kVK_F10] = CCKey::F10;
+    __key_map[kVK_F11] = CCKey::F11;
+    __key_map[kVK_F12] = CCKey::F12;
+    
+    __key_map[kVK_ANSI_Equal] = CCKey::Equals;
+    __key_map[kVK_ANSI_Minus] = CCKey::Minus;
+    __key_map[kVK_ANSI_Period] = CCKey::Period;
+    __key_map[kVK_ANSI_Comma] = CCKey::Comma;
+    __key_map[kVK_ANSI_Backslash] = CCKey::Backslash;
+    __key_map[kVK_ANSI_Slash] = CCKey::Slash;
+    __key_map[kVK_ANSI_Grave] = CCKey::Grave;
+    __key_map[kVK_ANSI_Quote] = CCKey::Apostrophe;
+    __key_map[kVK_ANSI_LeftBracket] = CCKey::LeftBracket;
+    __key_map[kVK_ANSI_RightBracket] = CCKey::RightBracket;
+    __key_map[kVK_ANSI_Semicolon] = CCKey::Semicolon;
+    
+    __key_map[kVK_Command] = CCKey::LeftWin;
+    __key_map[kVK_Control] = CCKey::Ctrl;
+    __key_map[kVK_Delete] = CCKey::Delete;
+    __key_map[kVK_CapsLock] = CCKey::CapsLock;
+    __key_map[kVK_DownArrow] = CCKey::Down;
+    __key_map[kVK_UpArrow] = CCKey::Up;
+    __key_map[kVK_LeftArrow] = CCKey::Left;
+    __key_map[kVK_RightArrow] = CCKey::Right;
+    __key_map[kVK_Shift] = CCKey::Shift;
+    __key_map[kVK_Space] = CCKey::Space;
+    __key_map[kVK_Tab] = CCKey::Tab;
+    __key_map[kVK_PageUp] = CCKey::PageUp;
+    __key_map[kVK_PageDown] = CCKey::PageDown;
+    __key_map[kVK_Option] = CCKey::Alt;
+    __key_map[kVK_Home] = CCKey::Home;
+}
+
 - (id)init {
     
     CCEventDispatcher* dispatcher = [CCEventDispatcher sharedDispatcher];
@@ -25,6 +109,7 @@ static CCMacEventDelegate* delegate = [[CCMacEventDelegate alloc] init];
     [dispatcher addKeyboardDelegate:self priority:1];
     [dispatcher addTouchDelegate:self priority:1];
     
+    [self __init_key_map];
 
     [super init];
     return self;
@@ -433,91 +518,7 @@ enum {
  ***/
 
 -(int) translatKey:(unsigned short)cocoa_key {
-    switch (cocoa_key) {
-        case kVK_ANSI_A:    return CCKey::A;
-        case kVK_ANSI_B:    return CCKey::B;
-        case kVK_ANSI_C:    return CCKey::C;
-        case kVK_ANSI_D:    return CCKey::D;
-        case kVK_ANSI_E:    return CCKey::E;
-        case kVK_ANSI_F:    return CCKey::F;
-        case kVK_ANSI_G:    return CCKey::G;
-        case kVK_ANSI_H:    return CCKey::H;
-        case kVK_ANSI_I:    return CCKey::I;
-        case kVK_ANSI_J:    return CCKey::J;
-        case kVK_ANSI_K:    return CCKey::K;
-        case kVK_ANSI_L:    return CCKey::L;
-        case kVK_ANSI_M:    return CCKey::M;
-        case kVK_ANSI_N:    return CCKey::N;
-        case kVK_ANSI_O:    return CCKey::O;
-        case kVK_ANSI_P:    return CCKey::P;
-        case kVK_ANSI_Q:    return CCKey::Q;
-        case kVK_ANSI_R:    return CCKey::R;
-        case kVK_ANSI_S:    return CCKey::S;
-        case kVK_ANSI_T:    return CCKey::T;
-        case kVK_ANSI_U:    return CCKey::U;
-        case kVK_ANSI_V:    return CCKey::V;
-        case kVK_ANSI_W:    return CCKey::W;
-        case kVK_ANSI_X:    return CCKey::X;
-        case kVK_ANSI_Y:    return CCKey::Y;
-        case kVK_ANSI_Z:    return CCKey::Z;
-            
-        case kVK_ANSI_1:    return CCKey::Num1;
-        case kVK_ANSI_2:    return CCKey::Num2;
-        case kVK_ANSI_3:    return CCKey::Num3;
-        case kVK_ANSI_4:    return CCKey::Num4;
-        case kVK_ANSI_5:    return CCKey::Num5;
-        case kVK_ANSI_6:    return CCKey::Num6;
-        case kVK_ANSI_7:    return CCKey::Num7;
-        case kVK_ANSI_8:    return CCKey::Num8;
-        case kVK_ANSI_9:    return CCKey::Num9;
-        case kVK_ANSI_0:    return CCKey::Num0;
-        
-        case kVK_F1:    return CCKey::F1;
-        case kVK_F2:    return CCKey::F2;
-        case kVK_F3:    return CCKey::F3;
-        case kVK_F4:    return CCKey::F4;
-        case kVK_F5:    return CCKey::F5;
-        case kVK_F6:    return CCKey::F6;
-        case kVK_F7:    return CCKey::F7;
-        case kVK_F8:    return CCKey::F8;
-        case kVK_F9:    return CCKey::F9;
-        case kVK_F10:    return CCKey::F10;
-        case kVK_F11:    return CCKey::F11;
-        case kVK_F12:    return CCKey::F12;
-            
-        case kVK_ANSI_Equal:    return CCKey::Equals;
-        case kVK_ANSI_Minus:    return CCKey::Minus;
-        case kVK_ANSI_Period:    return CCKey::Period;
-        case kVK_ANSI_Comma:    return CCKey::Comma;
-        case kVK_ANSI_Backslash:    return CCKey::Backslash;
-        case kVK_ANSI_Slash:    return CCKey::Slash;
-        case kVK_ANSI_Grave:    return CCKey::Grave;
-        case kVK_ANSI_Quote:    return CCKey::Apostrophe;
-        case kVK_ANSI_LeftBracket:    return CCKey::LeftBracket;
-        case kVK_ANSI_RightBracket:    return CCKey::RightBracket;
-        case kVK_ANSI_Semicolon:    return CCKey::Semicolon;
-        
-        case kVK_Command:    return CCKey::LeftWin;
-        case kVK_Control:    return CCKey::Ctrl;
-        case kVK_Delete:    return CCKey::Delete;
-        case kVK_CapsLock:    return CCKey::CapsLock;
-        case kVK_DownArrow:    return CCKey::Down;
-        case kVK_UpArrow:    return CCKey::Up;
-        case kVK_LeftArrow:    return CCKey::Left;
-        case kVK_RightArrow:    return CCKey::Right;
-        case kVK_Shift:    return CCKey::Shift;
-        case kVK_Space:    return CCKey::Space;
-        case kVK_Tab:    return CCKey::Tab;
-        case kVK_PageUp:    return CCKey::PageUp;
-        case kVK_PageDown:    return CCKey::PageDown;
-        case kVK_Option:    return CCKey::Alt;
-        case kVK_Home:    return CCKey::Home;
-            break;
-            
-        default:
-            break;
-    }
-    return 0;
+    return __key_map[cocoa_key];
 }
 
 -(BOOL) ccKeyUp:(NSEvent*)event {
